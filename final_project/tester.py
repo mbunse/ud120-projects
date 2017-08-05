de3d052707ee31e2bda83a825cc007dfae944293
Moritz Bunse <mbunse@hotmail.com>
2017-08-05 13:52:51 +0200

    Updated tester module to work with classifier and data

--- final_project/tester.py
+++ final_project/tester.py
@@ -12,9 +12,6 @@
 
 import pickle
 import sys
-from sklearn.cross_validation import StratifiedShuffleSplit
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
 
 PERF_FORMAT_STRING = "\
 \tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
@@ -23,14 +20,20 @@ RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFals
 \tFalse negatives: {:4d}\tTrue negatives: {:4d}"
 
 def test_classifier(clf, dataset, feature_list, folds = 1000):
-    data = featureFormat(dataset, feature_list, sort_keys = True)
-    labels, features = targetFeatureSplit(data)
-    cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+    from sklearn.model_selection import StratifiedShuffleSplit
+    #from sklearn.cross_validation import StratifiedShuffleSplit as StratifiedShuffleSplit_deprecated
+    #StratifiedShuffleSplit_deprecated(labels, )
+    #sys.path.append("../tools/")
+    from poi_id import extract_labels_features
+
+    #data = featureFormat(dataset, feature_list, sort_keys = True)
+    labels, features, _ = extract_labels_features(dataset)
+    cv = StratifiedShuffleSplit(n_splits=folds, random_state = 42)
     true_negatives = 0
     false_negatives = 0
     true_positives = 0
     false_positives = 0
-    for train_idx, test_idx in cv: 
+    for train_idx, test_idx in cv.split(features, labels): 
         features_train = []
         features_test  = []
         labels_train   = []
@@ -102,4 +105,7 @@ def main():
     test_classifier(clf, dataset, feature_list)
 
 if __name__ == '__main__':
+    # Import functions to be able to pickle load classifier
+    from poi_id import SelectMatchFeatures, SelectFeatureList, log_trans
+
     main()
